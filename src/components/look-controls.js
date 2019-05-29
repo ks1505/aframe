94bcaa81b14c1fddd7352f4e8ca9d73e27e493c6
ks1505 <49343115+ks1505@users.noreply.github.com>
2019-05-29 07:55:49 -0700

    Update look-controls.js

--- src/components/look-controls.js
+++ src/components/look-controls.js
@@ -141,13 +141,6 @@ module.exports.Component = registerComponent('look-controls', {
     // sceneEl events.
     sceneEl.addEventListener('enter-vr', this.onEnterVR);
     sceneEl.addEventListener('exit-vr', this.onExitVR);
-    window.addEventListener('devicemotion', function(){
-            var acc = event.acceleration;
-            this.el.object3D.position.x += acc.x*9.8;
-            this.el.object3D.position.y += acc.y*9.8;
-            this.el.object3D.position.z += acc.z*9.8;
-
-        }, true);
 
     // Pointer Lock events.
     if (this.data.pointerLockEnabled) {
@@ -352,7 +345,14 @@ module.exports.Component = registerComponent('look-controls', {
   onEnterVR: function () {
     if (!this.el.sceneEl.checkHeadsetConnected()) { return; }
     this.saveCameraPose();
-    this.el.object3D.position.set(0, 0, 0);
+    tick: function (){
+      window.addEventListener('devicemotion', function(){
+        var acc = event.acceleration;
+        this.el.object3D.position.x += acc.x*9.8;
+        this.el.object3D.position.y += acc.y*9.8;
+        this.el.object3D.position.z += acc.z*9.8;
+      }, true);
+    }
     this.el.object3D.updateMatrix();
   },
 
@@ -431,7 +431,7 @@ module.exports.Component = registerComponent('look-controls', {
     if (!this.hasSavedPose) { return; }
 
     // Reset camera orientation.
-    //el.object3D.position.copy(savedPose.position);
+    el.object3D.position.copy(savedPose.position);
     el.object3D.rotation.copy(savedPose.rotation);
     this.hasSavedPose = false;
   }
